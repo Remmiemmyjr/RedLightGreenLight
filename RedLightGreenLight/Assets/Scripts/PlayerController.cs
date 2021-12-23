85b121d58d743b90d3a4b8d917f0380e5c788713
Remmiemmyjr <emily@isimar.com>
2021-12-23 01:13:33 -0800

    added camera bobbing, needs work since player can no longer change y position

--- RedLightGreenLight/Assets/Scripts/PlayerController.cs
+++ RedLightGreenLight/Assets/Scripts/PlayerController.cs
@@ -8,23 +8,14 @@ public class PlayerController : MonoBehaviour
 
     public Transform playerCam = null;
 
-    public float sensitivity = 4f;
-
-    [Range(0f, 0.5f)] float moveSmoothTime = 0.15f;
-    [Range(0f, 0.5f)] float mouseSmoothTime = 0.05f;
-    float xRotation;
-    float walkspeed = 6f;
-    float gravity = -25f;
-    float velY = 0f;
-
-    Vector2 currDir = Vector2.zero;
-    Vector2 currVel = Vector2.zero;
-    Vector2 currMouseDel = Vector2.zero;
-    Vector2 currMouseVel = Vector2.zero;
+    float ogCamPosY;
 
     bool lockCursor = true;
+
     void Start()
     {
+        ogCamPosY = playerCam.position.y;
+
         controller = GetComponent<CharacterController>();
         if (lockCursor)
         {
@@ -33,25 +24,49 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         UpdateLook();
+
         UpdateMovement();
+
+        UpdateBobbing();
     }
 
+
+    // MOUSE LOOK VARIABLES ===========================
+    float xRotation;
+    Vector2 currMouseDel = Vector2.zero;
+    Vector2 currMouseVel = Vector2.zero;
+    // ================================================
     void UpdateLook()
     {
         Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
         currMouseDel = Vector2.SmoothDamp(currMouseDel, mouseDelta, ref currMouseVel, mouseSmoothTime);
 
-        xRotation -= currMouseDel.y * sensitivity;
+        xRotation -= currMouseDel.y * mouseSensitivity;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         playerCam.localEulerAngles = Vector3.right * xRotation;
-        transform.Rotate(Vector3.up * currMouseDel.x * sensitivity);
+        transform.Rotate(Vector3.up * currMouseDel.x * mouseSensitivity);
     }
 
+
+    // PLAYER MOVEMENT VARIABLES ======================
+    public float mouseSensitivity = 4f;
+
+    [Range(0f, 0.5f)] float moveSmoothTime = 0.15f;
+    [Range(0f, 0.5f)] float mouseSmoothTime = 0.05f;
+    float walkspeed = 6f;
+    float gravity = -25f;
+    float downwardVel = 0f;
+
+    Vector2 currDir = Vector2.zero;
+    Vector2 currVel = Vector2.zero;
+
+    bool isMoving;
+    // ================================================
     void UpdateMovement()
     {
         Vector2 dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
@@ -61,12 +76,44 @@ public class PlayerController : MonoBehaviour
 
         if(controller.isGrounded)
         {
-            velY = 0;
+            downwardVel = 0;
         }
 
-        velY += gravity * Time.deltaTime;
+        downwardVel += gravity * Time.deltaTime;
 
-        Vector3 vel = (transform.forward * currDir.y + transform.right * currDir.x) * walkspeed + (Vector3.up * velY);
+        Vector3 vel = (transform.forward * currDir.y + transform.right * currDir.x) * walkspeed + (Vector3.up * downwardVel);
         controller.Move(vel * Time.deltaTime);
+
+        if (Mathf.Abs(vel.x) > 0.1f || Mathf.Abs(vel.z) > 0.1f)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+    }
+
+
+    // PLAYER MOVEMENT VARIABLES ======================
+    public float bobSpeed = 10f;
+    public float bobAmount = 0.05f;
+
+    float timer = 0;
+    // ================================================
+    void UpdateBobbing()
+    {
+        if (isMoving)
+        {
+            timer += Time.deltaTime * bobSpeed;
+            playerCam.transform.position = new Vector3(playerCam.position.x, ogCamPosY + Mathf.Sin(timer) * bobAmount, 
+                playerCam.position.z);
+        }
+        else
+        {
+            timer = 0;
+            playerCam.transform.position = new Vector3(playerCam.position.x, Mathf.Lerp(playerCam.position.y, ogCamPosY,
+                Time.deltaTime * bobSpeed), playerCam.position.z);
+        }
     }
 }
