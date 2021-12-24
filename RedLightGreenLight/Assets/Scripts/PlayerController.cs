738e74b953f0a2481c1b0ff107d6dc0b2a01ed87
Remmiemmyjr <emily@isimar.com>
2021-12-23 17:09:31 -0800

    fucked with light made sprinting and walking bobbing

--- RedLightGreenLight/Assets/Scripts/PlayerController.cs
+++ RedLightGreenLight/Assets/Scripts/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        ogCamPosY = playerCam.position.y;
+        ogCamPosY = playerCam.localPosition.y;
 
         controller = GetComponent<CharacterController>();
         if (lockCursor)
@@ -28,6 +28,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         UpdateLook();
+
+        UpdateInput();
 
         UpdateMovement();
 
@@ -53,17 +55,28 @@ public class PlayerController : MonoBehaviour
     }
 
 
+    // PLAYER INPUT VARIABLES =========================
+    bool isSprinting;
+    // ================================================
+    void UpdateInput()
+    {
+        isSprinting = Input.GetKey(KeyCode.LeftShift);
+    }
+
+
     // PLAYER MOVEMENT VARIABLES ======================
     public float mouseSensitivity = 4f;
 
     [Range(0f, 0.5f)] float moveSmoothTime = 0.15f;
     [Range(0f, 0.5f)] float mouseSmoothTime = 0.05f;
-    float walkspeed = 6f;
+    float walkSpeed = 4f;
+    float sprintSpeed = 7.5f;
     float gravity = -25f;
     float downwardVel = 0f;
 
     Vector2 currDir = Vector2.zero;
     Vector2 currVel = Vector2.zero;
+    Vector2 currInput = Vector2.zero;
 
     bool isMoving;
     // ================================================
@@ -81,7 +94,7 @@ public class PlayerController : MonoBehaviour
 
         downwardVel += gravity * Time.deltaTime;
 
-        Vector3 vel = (transform.forward * currDir.y + transform.right * currDir.x) * walkspeed + (Vector3.up * downwardVel);
+        Vector3 vel = (transform.forward * currDir.y + transform.right * currDir.x) * (isSprinting ? sprintSpeed : walkSpeed) + (Vector3.up * downwardVel);
         controller.Move(vel * Time.deltaTime);
 
         if (Mathf.Abs(vel.x) > 0.1f || Mathf.Abs(vel.z) > 0.1f)
@@ -96,24 +109,31 @@ public class PlayerController : MonoBehaviour
 
 
     // PLAYER MOVEMENT VARIABLES ======================
-    public float bobSpeed = 10f;
-    public float bobAmount = 0.05f;
+    public float bobWalkSpeed = 10f;
+    public float bobSprintSpeed = 16f;
+    public float bobWalkAmount = 0.045f;
+    public float bobSprintAmount = 0.065f;
 
     float timer = 0;
     // ================================================
     void UpdateBobbing()
     {
+        if(!controller.isGrounded)
+        {
+            return;
+        }
+
         if (isMoving)
         {
-            timer += Time.deltaTime * bobSpeed;
-            playerCam.transform.position = new Vector3(playerCam.position.x, ogCamPosY + Mathf.Sin(timer) * bobAmount, 
-                playerCam.position.z);
+            timer += Time.deltaTime * (isSprinting ? bobSprintSpeed : bobWalkSpeed);
+            playerCam.transform.localPosition = new Vector3(playerCam.transform.localPosition.x, ogCamPosY + 
+                Mathf.Sin(timer) * (isSprinting ? bobSprintAmount : bobWalkAmount), playerCam.transform.localPosition.z);
         }
         else
         {
             timer = 0;
-            playerCam.transform.position = new Vector3(playerCam.position.x, Mathf.Lerp(playerCam.position.y, ogCamPosY,
-                Time.deltaTime * bobSpeed), playerCam.position.z);
+            //playerCam.transform.position = new Vector3(playerCam.position.x, Mathf.Lerp(playerCam.position.y, ogCamPosY,
+                //Time.deltaTime * bobSpeed), playerCam.position.z);
         }
     }
 }
