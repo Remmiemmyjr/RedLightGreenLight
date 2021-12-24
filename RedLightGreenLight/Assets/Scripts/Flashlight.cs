809b7432e4e4b3e653a4818bfe37bf1cb94c2e8f
Remmiemmyjr <emily@isimar.com>
2021-12-23 17:51:51 -0800

    flashlight is toggleable now

---
+++ RedLightGreenLight/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light flashlight;
+    public Transform camPos;
+    //float followSpeed = 0.01f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            flashlight.enabled = !flashlight.enabled;
+        }
+        //if(flashlight.enabled)
+        //{
+        //    flashlight.transform.localRotation = Quaternion.Slerp(flashlight.transform.localRotation, 
+        //        camPos.transform.localRotation, followSpeed * Time.deltaTime);
+        //}
+    }
+}
